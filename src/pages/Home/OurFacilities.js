d08587fc58c7c30fd645257b67b9d4a96b9303f6
Safiul-Azam <safiulazamriad@gmail.com>
2022-09-01 16:40:47 +0600

    load data through useFetch hooks

--- src/pages/Home/OurFacilities.js
+++ src/pages/Home/OurFacilities.js
@@ -1,20 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import useFetch from '../../hooks/useFetch';
 import OurFacility from './OurFacility';
 
 const OurFacilities = () => {
-    const [services, setServices] = useState([])
-    useEffect(() => {
-        fetch('http://localhost:5000/facilities')
-            .then(res => res.json())
-            .then(data => setServices(data))
-    }, [])
+    const {data, loading, error} = useFetch('http://localhost:5000/api/facilities')
     return (
         <div className='w-[1150px] mx-auto my-28'>
             <p style={{ letterSpacing: '5px' }} className='text-lg uppercase mb-4'>OUR SERVICES</p>
             <h2 className='text-5xl mb-7 text-black'>Hotel Facilities</h2>
             <div className='grid grid-cols-3 gap-8'>
                 {
-                    services.map(service => <OurFacility
+                    data.map(service => <OurFacility
                     key={service.id}
                     service={service}
                     ></OurFacility>)
--- src/pages/Home/OurFacility.js
+++ src/pages/Home/OurFacility.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const OurFacility = ({service}) => {
-    const {serviceName, description, img} = service
+    const {title, description, photo} = service
     return (
         <div className='border p-10'>
-            <img className='mb-2' src={img} alt="" />
-            <h2 className='text-xl mb-2'>{serviceName}</h2>
+            <img className='mb-2' src={photo} alt="" />
+            <h2 className='text-xl mb-2'>{title}</h2>
             <p className='text-lg tracking-normal text-[#777]'>{description}</p>
         </div>
     );
