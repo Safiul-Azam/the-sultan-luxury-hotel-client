654b5357d41210540a080785b06aca03cf8e8d68
Safiul-Azam <safiulazamriad@gmail.com>
2022-09-03 21:34:13 +0600

    install stripe  for payment method

--- src/pages/Booking/Payment.js
+++ src/pages/Booking/Payment.js
@@ -3,6 +3,11 @@ import { useLocation } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
 import Navbar from '../Shared/Navbar';
 import Pricing from './Pricing';
+import img1 from '../../images/banner/3.jpg'
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 
 const Payment = () => {
     const location = useLocation()
@@ -17,7 +22,7 @@ const Payment = () => {
         title,
         roomNumbers } = data
     return (
-        <div className='w-[1100px] mx-auto'>
+        <div>
             <div className=' pt-8 mix-blend-normal bg-black-400' style={
                 {
                     background: `linear-gradient(rgb(0,0,0,0.3),rgb(0,0,0,0.3)),url(${img1})`,
@@ -26,16 +31,19 @@ const Payment = () => {
                 }
             }>
                 <Navbar></Navbar>
-
-
                 <div className='text-left text-white w-[1150px] py-32 mx-auto'>
                     <p style={{ letterSpacing: '5px' }} className='text-lg uppercase mb-6'>LUXURY HOTEL</p>
                     <h2 style={{ lineHeight: '30px' }} className='text-6xl'>Payment Selection</h2>
                 </div>
 
             </div>
-            <div className='flex '>
-                <div></div>
+            <div className='w-[1100px] mx-auto flex'>
+                <div className='border'>
+
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
+                </div>
                 <div>
                     <div className='shadow-lg p-8'>
                         <Pricing
