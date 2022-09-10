3bc03623dc3a6706919b62788517176ba7e63f26
Safiul-Azam <safiulazamriad@gmail.com>
2022-09-10 18:09:02 +0600

    banner is done for responsive

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,32 +1,68 @@
-import React from 'react';
-import slider1 from '../../images/banner/3.jpg'
-import Navbar from '../Shared/Navbar';
 import { GrStar } from "react-icons/gr";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+// import { Autoplay, Pagination } from "swiper";
+import banner1 from '../../images/banner/1.jpg'
+import banner2 from '../../images/banner/2.jpg'
+import banner3 from '../../images/banner/3.jpg'
+import Navbar from '../Shared/Navbar';
+const banners = [
+  { id: 1, img: banner1 },
+  { id: 2, img: banner2 },
+  { id: 3, img: banner3 },
+]
 const Banner = () => {
   return (
-    <div className='lg:h-screen pt-32 mix-blend-multiply bg-black-400' style={
-      {
-        background: `linear-gradient(rgb(0,0,0,0.3),rgb(0,0,0,0.3)),url(${slider1})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat:'no-repeat'
-      }
-    }>
-      <Navbar></Navbar>
-      <div className='text-white text-center mt-12'>
-        <div className='flex justify-center text-xl mb-6 text-secondary'>
-          <GrStar />
-          <GrStar />
-          <GrStar />
-          <GrStar />
-          <GrStar />
-        </div>
-        <p style={{letterSpacing:'6px'}} className='text-lg uppercase mb-6'>The Ultimate Luxury Experience</p>
-        <h2 style={{letterSpacing:'12px', lineHeight:'70px'}} className='text-6xl uppercase lg:w-3/4 w-full mx-auto mb-7'>Enjoy The Best Moments of Life</h2>
-        <button className=" bg-transparent tracking-widest border px-5 py-3 uppercase"><a href="#room&suites">Rooms & Suites</a></button>
+      <div className="w-full">
+        <Navbar></Navbar>
+        <Swiper
+          centeredSlides={true}
+          slidesPerView={1}
+          slidesPerGroup={1}
+          // autoplay={{
+          //   delay: 3500,
+          //   disableOnInteraction: false,
+          // }}
+
+          loop={true}
+          loopFillGroupWithBlank={true}
+          pagination={{
+            clickable: true,
+          }}
+          // modules={[Autoplay, Pagination]}
+          className="mySwiper h-screen"
+        >
+
+          {
+            banners.map(banner => <SwiperSlide
+              key={banner._id}
+              className='pt-6'
+              style={
+                {
+                  background: `linear-gradient(rgb(0,0,0,0.3),rgb(0,0,0,0.3)),url(${banner.img})`,
+                  backgroundPosition: 'center',
+                  backgroundSize: 'cover'
+                }
+              }
+            >
+              <Navbar></Navbar>
+              <div className='text-white text-center mt-16 lg:mt-40'>
+                <div className='flex justify-center lg:text-xl text-sm mb-6 text-secondary'>
+                  <GrStar />
+                  <GrStar />
+                  <GrStar />
+                  <GrStar />
+                  <GrStar />
+                </div>
+                <p style={{ letterSpacing: '6px' }} className='lg:text-lg text-sm uppercase mb-6'>The Ultimate Luxury Experience</p>
+                <h2 style={{ letterSpacing: '12px' }} className='lg:text-6xl md:text-3xl text-2xl uppercase lg:w-3/4 w-full mx-auto mb-7 leading-loose'>Enjoy The Best Moments of Life</h2>
+                <button className=" bg-transparent tracking-widest border px-5 py-3 lg:my-0 my-5 uppercase"><a href="#room&suites">Rooms & Suites</a></button>
+              </div>
+            </SwiperSlide>)
+          }
+        </Swiper>
       </div>
-    </div>
   );
 };
 
--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -6,36 +6,38 @@ import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 
 const Navbar = () => {
-    const {user, dispatch} = useContext(AuthContext)
-    const logOut = ()=>{
-        dispatch({type:'LOGOUT'})
+    const { user, dispatch } = useContext(AuthContext)
+    const logOut = () => {
+        dispatch({ type: 'LOGOUT' })
         localStorage.removeItem()
     }
     const menu = <>
-    <li><CustomLink to='/'>HOME</CustomLink></li>
-    <li><CustomLink to='/about'>ABOUT</CustomLink></li>
-    <li><CustomLink to='/room'>ROOM & SUITES</CustomLink></li>
-    <li><CustomLink to='/restaurant'>RESTAURANT</CustomLink></li>
-    <li><CustomLink to='/spa'>SPA</CustomLink></li>
-    <li><CustomLink to='/contact-us'>CONTACT</CustomLink></li>
-    { user?
-        <li><CustomLink to='/login'><button onClick={logOut}>SIGN OUT</button></CustomLink></li>:
-        <li><CustomLink to='/login'>LOGIN</CustomLink></li>}
+        <li><CustomLink to='/'>HOME</CustomLink></li>
+        <li><CustomLink to='/about'>ABOUT</CustomLink></li>
+        <li><CustomLink to='/room'>ROOM & SUITES</CustomLink></li>
+        <li><CustomLink to='/restaurant'>RESTAURANT</CustomLink></li>
+        <li><CustomLink to='/spa'>SPA</CustomLink></li>
+        <li><CustomLink to='/contact-us'>CONTACT</CustomLink></li>
+        {user ?
+            <li><CustomLink to='/login'><button onClick={logOut}>SIGN OUT</button></CustomLink></li> :
+            <li><CustomLink to='/login'>LOGIN</CustomLink></li>}
     </>
-    
+
     return (
-        <div className='  lg:w-[1200px] mx-auto z-10 absolute top-4 left-16'>
+        <div className='lg:w-[1200px] mx-auto z-10 absolute top-3 left-16'>
             <div className="navbar">
-                <div className="navbar-start">
+                <div className="space-x-32 lg:space-x-0 lg:navbar-start">
                     <div className="dropdown">
-                        <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <label tabIndex="0" className="btn btn-white lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52">
-                           {menu}
+                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box">
+                            {menu}
                         </ul>
                     </div>
-                    <Link to='/' className="normal-case text-center"><span className='lg:text-3xl text-xl block text-primary font-medium lg:h-8'>THE SULTAN</span><p style={{letterSpacing:'8px',fontSize:'10px'}} className='text-white'>LUXURY HOTEL</p></Link>
+                    <div className=''>
+                        <Link to='/' className="normal-case text-center"><span className='lg:text-3xl text-xl block text-primary font-medium lg:h-8'>THE SULTAN</span><p style={{ letterSpacing: '8px', fontSize: '10px' }} className='text-white'>LUXURY HOTEL</p></Link>
+                    </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0 text-lg tracking-widest ">
