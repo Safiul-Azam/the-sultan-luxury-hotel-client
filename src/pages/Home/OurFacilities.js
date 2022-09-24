f8a69b508d31a43d16c930746b45aa3cefd399da
Safiul-Azam <safiulazamriad@gmail.com>
2022-09-24 20:49:38 +0600

    our facility component is deleted

--- src/pages/Home/OurFacilities.js
+++ src/pages/Home/OurFacilities.js
@@ -5,7 +5,7 @@ import OurFacility from './OurFacility';
 const OurFacilities = () => {
     const {data, loading} = useFetch('https://sultan-hotel-1.onrender.com/api/facilities')
     if(loading){
-        return<Spinner></Spinner>
+     return <Spinner/>
     }
     return (
         <div className='container mx-auto lg:py-24 py-12 px-4'>
@@ -13,11 +13,11 @@ const OurFacilities = () => {
             <h2 className='lg:text-5xl text-3xl mb-7'>Hotel Facilities</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
                 {
-                    data.map(service => <OurFacility
-                    key={service._id}
-                    service={service}
-                    loading={loading}
-                    ></OurFacility>)
+                    data.map(service => <div className='border p-6 hover:duration-500 hover:ease-out ease-in-out duration-500 hover:shadow-xl'>
+                    <img className='mb-2' src={service.photo} alt="" />
+                    <h2 className='text-xl mb-2'>{service.title}</h2>
+                    <p className='text-lg tracking-normal text-[#777]'>{service.description}</p>
+                </div>)
                 }
             </div>
         </div>
